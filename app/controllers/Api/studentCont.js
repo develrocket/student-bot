f6748aac84aafb370c4455d16840832d955e3016
xiaojang <fastmoon66@outlook.com>
2022-05-04 15:03:02 +0800

    update

--- app/controllers/Api/studentCont.js
+++ app/controllers/Api/studentCont.js
@@ -5,19 +5,34 @@ const StudentResultModel = require('../../models/studentResult');
 const SessionModel = require('../../models/sessionResult');
 const axios = require('axios').default;
 
+const fetchSession = async function() {
+    let sessions = await SessionModel.find().sort({session_no: -1}).limit(1);
+    let lastId = sessions[0].session_no;
+    try {
+        let res = await axios.get('https://fortunaenglish.com/api/fetch/livesession?lastId=' + lastId);
+
+        for (const sessionItem of res.data) {
+            const newSessionItem = new SessionModel();
+            newSessionItem.language = sessionItem.language;
+            newSessionItem.session_type = sessionItem.type;
+            newSessionItem.session_name = sessionItem.session_name;
+            newSessionItem.session_no = sessionItem.id;
+            newSessionItem.session_start = sessionItem.start_time;
+            newSessionItem.questions_no = sessionItem.questions;
+            await newSessionItem.save();
+        }
+        console.log('insert-new-session:', res.data.length);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = function(){
 
     return {
         fetchSession: async function(req, res) {
-            let sessions = await SessionModel.find().sort({session_no: -1}).limit(1);
-            let lastId = sessions[0].session_no;
-            console.log(lastId);
-            try {
-                let res = await axios.get('https://fortunaenglish.com/api/fetch/livesession?lastId=' + lastId);
-                console.log(res.data);
-            } catch (err) {
-                console.log(err);
-            }
+            await fetchSession();
+            return res.json({result: "success"});
         },
 
         filter: async function (req, res) {
